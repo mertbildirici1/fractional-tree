3103619090e77eb738ea573ef24e94049a616111
Mert Bildirici <hmertbildirici@gmail.com>
2024-07-04 19:01:48 -0400

    colored it

--- sketch.js
+++ sketch.js
@@ -1,31 +1,29 @@
-// Fractal Tree
-// The Coding Train / Daniel Shiffman
-// https://thecodingtrain.com/challenges/14-fractal-trees-recursive
-// https://youtu.be/0jjeOYMjmDU
-
-// https://editor.p5js.org/codingtrain/sketches/xTjmYXU3q
-
 let angle = 0;
 let slider;
 
 function setup() {
   createCanvas(640, 360);
-  slider = createSlider(0, TWO_PI, PI / 4, 0.01);
+  slider = select('#angleSlider');
+  slider.input(updateAngle);
+}
+
+function updateAngle() {
+  angle = slider.value();
 }
 
 function draw() {
   background(0);
-  angle = slider.value();
   stroke(255);
-  strokeWeight(1);
   translate(width * 0.5, height);
   branch(100);
 }
 
 function branch(len) {
+  strokeWeight(map(len, 10, 100, 1, 8)); // Varying stroke weight
+  stroke(map(len, 10, 100, 255, 100), map(len, 10, 100, 200, 50), map(len, 10, 100, 150, 50)); // Color gradient
   line(0, 0, 0, -len);
   translate(0, -len);
-  if (len > 4) {
+  if (len > 10) {
     push();
     rotate(angle);
     branch(len * 0.67);
@@ -34,5 +32,10 @@ function branch(len) {
     rotate(-angle);
     branch(len * 0.67);
     pop();
+  } else {
+    // Draw simple leaves
+    fill(34, 139, 34, 200); // Forest green with some transparency
+    noStroke();
+    ellipse(0, 0, 10, 20);
   }
 }
